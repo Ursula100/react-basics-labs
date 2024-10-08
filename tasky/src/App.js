a81719964beeec8b8ad7359ad0712c4ff8ca32ab
tamen <20100902@mail.wit.ie>
2024-10-08 11:43:02 +0100

    Add task: form added

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Task from './components/Task';
 import React, { useState } from 'react';
+import AddTaskForm from './components/Form';
 
 function App() {
   const [ taskState, setTaskState ] = useState({
@@ -33,7 +34,8 @@ function App() {
           markDone={() => doneHandler(index)}
           deleteTask = {() => deleteHandler(index)}
         />
-      ))} 
+      ))}
+      <AddTaskForm />
     </div>
   );
 }
